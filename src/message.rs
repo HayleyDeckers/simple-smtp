2cea8802496302dcd7ba38c9fb065437cee1fa75
Hayley <git@codingcat.nl>
2025-12-11 00:28:29 +0100

    ⭐ Add no_std and no_alloc compatible datetimes

---
+++ src/message.rs
@@ -0,0 +1,6 @@
+//! Message formatting utilities.
+//!
+//! This module provides utilities for formatting email messages according to RFC 5322.
+
+pub mod datetime;
+pub use datetime::{DateTime, TimeZone};
